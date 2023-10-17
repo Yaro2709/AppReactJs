eac2f95eec6756225f728d7cf2583bf3242ee20f
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-10-17 23:19:57 +0300

    Validaiya profile

--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -1,5 +1,13 @@
 import { Currency } from 'entities/Currency/model/types/currency';
-import { Country } from 'entities/Country';
+import { Country } from 'entities/Country/model/types/country';
+
+export enum ValidateProfileError {
+    INCORRECT_USER_DATA = 'INCORRECT_USER_DATA',
+    INCORRECT_AGE = 'INCORRECT_AGE',
+    INCORRECT_COUNTRY = 'INCORRECT_COUNTRY',
+    NO_DATA = 'NO_DATA',
+    SERVER_ERROR = 'SERVER_ERROR',
+}
 
 export interface Profile {
     first?: string;
@@ -14,8 +22,9 @@ export interface Profile {
 
 export interface ProfileSchema {
     data?: Profile;
-    form?: Profile; /* то, что наизменял сам польователь */
+    form?: Profile;
     isLoading: boolean;
     error?: string;
     readonly: boolean;
+    validateErrors?: ValidateProfileError[];
 }
