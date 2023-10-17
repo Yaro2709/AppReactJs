8c3c871e93783c24c44065a3cce210198de99fc1
Yaroslav Tsvira <51800453+Yaro2709@users.noreply.github.com>
2023-10-17 22:31:07 +0300

    Css moduls

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] { //не забываем про типизацию WebpackPluginInstance
 
@@ -9,6 +10,10 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
         new HtmlWebpackPlugin({
             template: paths.html, //объявляем шаблонный файл
         }), //плагин для html сборки
-        new webpack.ProgressPlugin(), //плагин для того, чтобы мы видели сколько процентов проекта мы уже собрали. 
+        new webpack.ProgressPlugin(), //плагин для того, чтобы мы видели сколько процентов проекта мы уже собрали.
+        new MiniCssExtractPlugin({ //плагин для css
+            filename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях
+            chunkFilename: 'css/[name].[contenthash:8].css', //делаем хеширование в названиях
+        })        
     ]
 }
